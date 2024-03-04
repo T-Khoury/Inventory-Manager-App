950abed2216fa1d8eb064496bc08658d6ecbf9de
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2024-03-04 09:31:31 -0500

    Set up controllers

---
+++ controllers/categoryController.js
@@ -0,0 +1,45 @@
+const Category = require("../models/category");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+
+// Display list of all categories
+exports.category_list = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Category List");
+});
+//Display detail page for specific category
+exports.category_detail = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Category detail page");
+});
+
+
+// Display category create form on GET.
+exports.category_create_get = (req, res, next) => {
+    // res.render("category_form", { title: "Create Category" });
+    res.send("NOT IMPLEMENTED: Category create form");
+}
+
+
+exports.category_create_post = [
+    body()
+]
+
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+    res.render("category_delete", {
+        title: "Delete Category"
+    })
+});
+
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+    res.render("category_delete", {
+        title: "Delete Category"
+    })
+});
+
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+})
+
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+})
---
+++ controllers/itemController.js
@@ -0,0 +1,48 @@
+const Item = require("../models/item");
+const asyncHandler = require("express-async-handler");
+
+
+exports.index = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Site Home Page")
+})
+
+// Display list of all itemss
+exports.item_list = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Item List");
+});
+//Display detail page for specific category
+exports.item_detail = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Item detail page");
+});
+
+
+// Display category create form on GET.
+exports.item_create_get = (req, res, next) => {
+    // res.render("category_form", { title: "Create Category" });
+    res.send("NOT IMPLEMENTED: Item create form");
+}
+
+
+exports.item_create_post = [
+    body()
+]
+
+exports.citem_delete_get = asyncHandler(async (req, res, next) => {
+    res.render("item_delete", {
+        title: "Delete Item"
+    })
+});
+
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+    res.render("item_delete", {
+        title: "Delete Item"
+    })
+});
+
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+})
+
+exports.item_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+})
